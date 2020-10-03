e9bc84dda952c916117ba2c3966853c77b158e16
ojvar <MOjvar.AmirHossein@GMail.com>
2020-10-03 20:08:32 +0330

    Add redis-helper

---
+++ src/core/heleprs/redis-helper.ts
@@ -0,0 +1,103 @@
+"use strict";
+
+import Redis from "redis";
+import GlobalData from "../global/global-data";
+
+/**
+ * Redis helper class
+ */
+export default class RedisHelper {
+    private _client: Redis.RedisClient;
+    private clientOptions: Redis.ClientOpts;
+
+    /**
+     * Redis class ctr
+     * @param option RedisOptionType Options
+     */
+    constructor(options?: Redis.ClientOpts) {
+        this.clientOptions = options;
+    }
+
+    /**
+     * Get redis client
+     * @returns Redis.RedisClient redis client
+     */
+    get client(): Redis.RedisClient {
+        return this._client;
+    }
+
+    /**
+     * Connect to server
+     */
+    public connect(): Promise<void> {
+        return new Promise((resolve, reject): void => {
+            this._client = Redis.createClient(this.clientOptions);
+
+            this.client.on("error", (err) => {
+                GlobalData.logger.error(JSON.stringify(err));
+                reject(err);
+            });
+
+            if (this.clientOptions.password) {
+                this.client.auth(
+                    this.clientOptions.password,
+                    (err: Error, reply: string) => {
+                        GlobalData.logger.error(JSON.stringify(err));
+                        reject(err);
+                    }
+                );
+            }
+
+            resolve();
+        });
+    }
+
+    /**
+     * Disconnect from server
+     */
+    public disconnect(): void {
+        if (this.client) {
+            this.client.end();
+        }
+    }
+
+    /**
+     * Change database
+     * @param db number|string DB index
+     */
+    public selectDB(db: number | string): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            /* Callback function */
+            const cb: Redis.Callback<string> = (err: Error, data: string) => {
+                if (err) {
+                    GlobalData.logger.error(JSON.stringify(err));
+                    reject(err);
+                } else {
+                    resolve(true);
+                }
+            };
+
+            this._client.select(db, cb);
+        });
+    }
+
+    /**
+     * Run a command - promise based
+     */
+    public send(cmd: string, ...args: string[]): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            /* Callback function */
+            const cb: Redis.Callback<string> = (err: Error, data: string) => {
+                if (err) {
+                    GlobalData.logger.error(JSON.stringify(err));
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            };
+
+            /* Send command */
+            this._client.sendCommand(cmd, args, cb);
+        });
+    }
+}
