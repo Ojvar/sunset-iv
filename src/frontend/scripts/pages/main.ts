f04e1ca8f807ef67ec923e2f16fb963d5347469b
ojvar <MOjvar.AmirHossein@GMail.com>
2020-10-12 20:14:26 +0330

    Update modules Remove Vue3 and install vue2

--- src/frontend/scripts/pages/main.ts
+++ src/frontend/scripts/pages/main.ts
@@ -1,35 +1,24 @@
-// "use strict";
+"use strict";
 
-// import App from "../core/app";
-// import Vue from "vue";
-// // import VueCompositionApi from "@vue/composition-api";
+import App from "../core/app";
 
-// /**
-//  * MainPage class
-//  */
-// export default class MainPage extends App {
-//     /**
-//      * Ctr
-//      */
-//     constructor() {
-//         super();
-//         this.init();
-//     }
+/**
+ * MainPage class
+ */
+export default class MainPage extends App {
+    /**
+     * Ctr
+     */
+    constructor() {
+        super();
+        this.init();
+    }
 
-//     /**
-//      * Init vue
-//      */
-//     private init(): void {
-//         console.log(Vue);
-//         // Vue.use(VueCompositionApi);
-//         // const app = createApp({});
-//         // app.mount("#app");
-//     }
-// }
+    /**
+     * Init vue
+     */
+    private init(): void {}
+}
 
-// /* Create a new instance */
-// new MainPage();
-
-import * as Vue from "vue";
-
-console.log(Vue);
+/* Create a new instance */
+new MainPage();
